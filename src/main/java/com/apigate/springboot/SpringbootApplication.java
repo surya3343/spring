06980da9a400e2ebfeda4ff759eb8b14bd28a51b
surya3343 <63227905+surya3343@users.noreply.github.com>
2020-04-27 19:14:18 +0530

    Update SpringbootApplication.java

--- src/main/java/com/apigate/springboot/SpringbootApplication.java
+++ src/main/java/com/apigate/springboot/SpringbootApplication.java
@@ -6,21 +6,12 @@ import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import io.prometheus.client.spring.boot.EnablePrometheusMetrics;
 //import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
-package org.springframework.metrics.export.prometheus;
-import org.springframework.context.annotation.Import;
-import org.springframework.metrics.boot.EnableMetrics;
-import java.lang.annotation.*;
 
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-@EnableMetrics
-@EnablePrometheusScraping
 
 
 @EnableSpringBootMetricsCollector
 @EnablePrometheusMetrics
-//@SpringBootApplication
+@SpringBootApplication
 
 public class SpringbootApplication {
 
