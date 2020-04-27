a4d3ca2a8a7c1d54df24fa8c3085fb8a467aadc9
surya3343 <63227905+surya3343@users.noreply.github.com>
2020-04-27 19:19:30 +0530

    Update SpringbootApplication.java

--- src/main/java/com/apigate/springboot/SpringbootApplication.java
+++ src/main/java/com/apigate/springboot/SpringbootApplication.java
@@ -2,15 +2,7 @@ package com.apigate.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
-import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
-import io.prometheus.client.spring.boot.EnablePrometheusMetrics;
-//import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 
-
-
-@EnableSpringBootMetricsCollector
-@EnablePrometheusMetrics
 @SpringBootApplication
 
 public class SpringbootApplication {
