61ffe827ca47c070c89aee2aad4a314f10e58566
surya3343 <63227905+surya3343@users.noreply.github.com>
2020-04-27 19:25:36 +0530

    Update HelloWorldController.java

--- src/main/java/com/apigate/springboot/HelloWorldController.java
+++ src/main/java/com/apigate/springboot/HelloWorldController.java
@@ -4,8 +4,7 @@ package com.apigate.springboot;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@EnablePrometheusMetrics
-@Timed
+
 
 public class HelloWorldController
 {
