7e11bd3171c5eb7049f3fb8d1f8273fe73a09cdb
surya3343 <63227905+surya3343@users.noreply.github.com>
2020-04-27 18:26:29 +0530

    Update SpringbootApplication.java

--- src/main/java/com/apigate/springboot/SpringbootApplication.java
+++ src/main/java/com/apigate/springboot/SpringbootApplication.java
@@ -16,7 +16,7 @@ import java.lang.annotation.*;
 @Documented
 @EnableMetrics
 @EnablePrometheusScraping
-@Import(PrometheusMetricsConfiguration.class)
+
 
 @EnableSpringBootMetricsCollector
 @EnablePrometheusMetrics
