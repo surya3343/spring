4f33b72d0025eedc2be6733fe20b0d8b1e4fe950
ishaq_shaikh <shaikhishaq1996@gmail.com>
2020-04-27 17:53:52 +0530

    commenting EnablePrometheusMetrics

--- src/main/java/com/apigate/springboot/SpringbootApplication.java
+++ src/main/java/com/apigate/springboot/SpringbootApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
-import io.prometheus.client.spring.boot.EnablePrometheusMetrics;
+// import io.prometheus.client.spring.boot.EnablePrometheusMetrics;
 import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 
 @EnableSpringBootMetricsCollector
-@EnablePrometheusMetrics
+// @EnablePrometheusMetrics
 @SpringBootApplication
 
 public class SpringbootApplication {
