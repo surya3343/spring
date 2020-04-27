68678b3d8d46d0f64ba11f152c993a96822dd7e6
surya3343 <63227905+surya3343@users.noreply.github.com>
2020-04-27 19:21:26 +0530

    Update HelloWorldController.java

--- src/main/java/com/apigate/springboot/HelloWorldController.java
+++ src/main/java/com/apigate/springboot/HelloWorldController.java
@@ -4,6 +4,9 @@ package com.apigate.springboot;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+@RestController
+@Timed
+
 public class HelloWorldController
 {
 @RequestMapping("/")
