9969dc87099d3627d3b1a2ba67ac4dfdb5d5f45d
nikhil313 <nikhil161096@gmail.com>
2020-04-30 00:19:59 +0530

    logger added

--- src/main/java/com/apigate/springboot/SpringbootApplication.java
+++ src/main/java/com/apigate/springboot/SpringbootApplication.java
@@ -1,13 +1,20 @@
 package com.apigate.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
 public class SpringbootApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SpringbootApplication.class);
+
 
 	public static void main(String[] args) {
+		logger.info("this is a info message");
+    logger.warn("this is a warn message");
+    logger.error("this is a error message");
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
